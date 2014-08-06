a8009194a8ae6f4bad0347e43c8be9c1192399c8
Steven Tang <stang12290@gmail.com>
2014-08-05 17:29:28 -0700

    add skeleton responsive framework

---
+++ assets/javascripts/icon-hover.js
@@ -0,0 +1,6 @@
+function hover(img) {
+    img.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
+}
+function unhover(img) {
+    img.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
+}
