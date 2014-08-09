e992d920888871e2e8c529f803921bb8094987d3
Steven Tang <stang12290@gmail.com>
2014-08-09 03:33:25 -0700

    complete problem div functionality with not mobile op

---
+++ assets/javascripts/animations.js
@@ -0,0 +1,34 @@
+//list menu icon animations
+
+$(document).ready(function(){
+  $(".mail-list").mouseover(function(){
+    $(".mail-icon").css("color", "#D77691");
+  });
+  $(".mail-list").mouseout(function(){
+    $(".mail-icon").css("color", "#A19DB6");
+  });
+  $(".problem-list").mouseover(function(){
+    $(".problem-icon").css("color", "#D77691");
+  });
+  $(".problem-list").mouseout(function(){
+    $(".problem-icon").css("color", "#A19DB6");
+  });
+
+//problem section slide animation
+
+  $('.problem-content').hide();
+    $('.problem-list').click(function () {
+      var $this = $(this);
+        $(this).siblings('.problem-content').slideToggle(150, function () {
+        });
+    });
+
+//check problem answer
+//***If you're smart enough to check the code for the answer, then you're just insulting your own intelligence by not solving it yourself, bitch**
+
+  $('#Answer').on('input', function() {
+    var answer = $(this).val();
+    if(answer == '10'){$('#Check-answer').text('Correct!').hide().fadeIn(150);}
+    else{$('#Check-answer').text('Nope!').hide().fadeIn(150);}
+  });
+});
