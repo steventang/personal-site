d85d1e117ff10fd2c0e41a1ad40c594514883224
Steven Tang <stang12290@gmail.com>
2014-08-12 19:25:28 -0700

    hide validation if input empty

--- assets/javascripts/animations.js
+++ assets/javascripts/animations.js
@@ -31,14 +31,17 @@ $(document).ready(function(){
     if(answer == '10'){
       $('.validate-wrong').hide();
       $('.validate-correct').css("display", "inline");
-      $('#Check-answer').text('Correct!').hide().fadeIn(150);
-      
+      $('#Check-answer').text('Correct!').hide().fadeIn(150);     
     }
     else{
       $('.validate-correct').hide();
       $('.validate-wrong').css("display", "inline");
       $('#Check-answer').text('Nope!').hide().fadeIn(150);
-      
+    }
+    if(answer == ''){
+      $('.validate-wrong').hide();
+      $('.validate-correct').hide();
+      $('#Check-answer').hide();
     }
   });
 });
